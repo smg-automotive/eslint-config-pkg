3854f699248e038769e5d132008b231985bf0c69
Quang Pham <quangphamit@gmail.com>
2022-05-09 13:08:29 +0700

    refactor: remove unused test runner

--- src/__tests__/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-${PWD}/src/__tests__/testRunner.sh -c ${PWD}/src/__tests__/.eslintrc.js -g 0 -b 15
-${PWD}/src/__tests__/testRunner.sh -c ${PWD}/src/__tests__/.eslintrc.react.js -g 0 -b 17
-${PWD}/src/__tests__/testRunner.sh -c ${PWD}/src/__tests__/.eslintrc.next.js -g 0 -b 17
