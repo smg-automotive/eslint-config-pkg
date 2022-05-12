5ffd91b3eb92f01e84036a8841a7b4986259eb01
Quang Pham <quangphamit@gmail.com>
2022-05-12 13:56:59 +0700

    chore: remove usage of prop-types; replace it with mock

--- src/__tests__/good/js/markup.jsx
+++ src/__tests__/good/js/markup.jsx
@@ -1,11 +1,10 @@
-import PropTypes from 'prop-types';
-
 const Subtitle = ({ children }) => (
   <h2 className="text-xl mt-20 mb-10 lg:mb-60 font-regular">{children}</h2>
 );
 
+// To simulate behavior of prop-types
 Subtitle.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: () => {},
 };
 
 export default Subtitle;
