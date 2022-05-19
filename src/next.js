0c132d3f78e4b0fe64902a25e13a8cd35bb8891f
boyarskiy <gekabrs@gmail.com>
2022-05-19 16:05:11 +0200

    fix: override rules for cypress and jest

--- src/next.js
+++ src/next.js
@@ -4,7 +4,19 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   overrides: [
     {
-      files: ['**/__tests__/pages/**/*.test.tsx'],
+      files: ['**/cypress/**/*'],
+      rules: {
+        'jest/expect-expect': 'off',
+        'jest/valid-expect-in-promise': 'off',
+        'jest/valid-expect': 'off',
+        'jest/no-standalone-expect': 'off',
+        'testing-library/await-async-query': 'off',
+        'testing-library/prefer-screen-queries': 'off',
+        'testing-library/await-async-utils': 'off',
+      },
+    },
+    {
+      files: ['**/components/**/*.test.tsx', '**/components/**/*.test.jsx'],
       rules: {
         'jest/valid-expect': 'off',
         'jest/expect-expect': 'off',
