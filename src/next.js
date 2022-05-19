7c5f0713309f6ac3c22e0066b8deb3bf30990ac4
boyarskiy <gekabrs@gmail.com>
2022-05-19 15:48:28 +0200

    fix: disable jest expect due to testing library

--- src/next.js
+++ src/next.js
@@ -2,6 +2,17 @@ module.exports = {
   extends: ['./react.js', 'next/core-web-vitals'],
   plugins: ['@next/eslint-plugin-next'],
   parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: [
+        "**/__tests__/pages/**/*.test.tsx",
+      ],
+      rules: {
+        "jest/valid-expect": "off",
+        "jest/expect-expect": "off",
+      },
+    },
+  ],
   rules: {
     'import/no-internal-modules': [
       'error',
