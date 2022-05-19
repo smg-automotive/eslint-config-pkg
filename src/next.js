5fb7ed2b6fa97d7f15a0c81942307802746aaef7
boyarskiy <gekabrs@gmail.com>
2022-05-19 15:42:46 +0200

    fix: allow internal modules for next

--- src/next.js
+++ src/next.js
@@ -2,4 +2,12 @@ module.exports = {
   extends: ['./react.js', 'next/core-web-vitals'],
   plugins: ['@next/eslint-plugin-next'],
   parser: '@typescript-eslint/parser',
+  rules: {
+    'import/no-internal-modules': [
+      'error',
+      {
+        allow: ['next/*'],
+      },
+    ],
+  },
 };
