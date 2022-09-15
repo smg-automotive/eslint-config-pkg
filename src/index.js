15f2bcd8a223414ca3a8b7a7486d957a0bc36339
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-09-15 15:16:44 +0200

    fix: use the no-shadow rule from typescript eslint (#76)

--- src/index.js
+++ src/index.js
@@ -156,6 +156,7 @@ module.exports = {
         ignoreRestSiblings: true,
       },
     ],
-    'no-shadow': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
   },
 };
