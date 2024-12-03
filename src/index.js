e3df53addf526926172ffc56b50d901d12053533
talamcol <colin@talamona.ch>
2024-12-03 15:45:44 +0100

    fix: check if we can remove index.js file

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as next } from './next.mjs';
-export { default as basic } from './index.mjs';
-export { default as react } from './react.mjs';
-export { default as prettier } from './prettier.mjs';
