b86d04cbff3e89d818a1380021c92f1d602c6740
Lorenzo Schürch <me@lnk.codes>
2026-06-22 17:05:56 +0200

    feat: require post method on JSX forms (#495)
    feat: require post method on jsx forms

---
+++ src/__tests__/good/ts/FormMethod.tsx
@@ -0,0 +1,9 @@
+const ContactForm = () => (
+  <form method="post">
+    <label htmlFor="email">Email</label>
+    <input id="email" name="email" />
+    <button type="submit">Submit</button>
+  </form>
+);
+
+export default ContactForm;
